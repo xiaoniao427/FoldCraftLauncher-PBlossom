056cb1278737fe9d88d65aa39577f58c03af5e34
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-04-14 21:01:29 +0800

    Update EulaFragment.java

--- FCL/src/main/java/com/tungsten/fcl/fragment/EulaFragment.java
+++ FCL/src/main/java/com/tungsten/fcl/fragment/EulaFragment.java
@@ -26,7 +26,7 @@ public class EulaFragment extends FCLFragment implements View.OnClickListener {
     private FCLProgressBar progressBar;
     private FCLTextView eula;
     private FCLButton next;
-    StatService.init(Context context, String appKey, String appChannel);
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -69,7 +69,6 @@ public class EulaFragment extends FCLFragment implements View.OnClickListener {
                 SharedPreferences sharedPreferences = getActivity().getSharedPreferences("launcher", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putBoolean("isFirstLaunch", false);
-                StatService.setAuthorizedState(getActivity(), true);
                 editor.apply();
                 ((SplashActivity) getActivity()).start();
             }
