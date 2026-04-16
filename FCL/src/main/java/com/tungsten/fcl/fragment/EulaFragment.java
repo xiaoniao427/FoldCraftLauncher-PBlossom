4d019c37ee01e64500343a1be3f72a232bcca5fb
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-04-17 00:05:54 +0800

    Update EulaFragment.java

--- FCL/src/main/java/com/tungsten/fcl/fragment/EulaFragment.java
+++ FCL/src/main/java/com/tungsten/fcl/fragment/EulaFragment.java
@@ -33,10 +33,6 @@ import java.io.IOException;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_eula, container, false);
 
-// SDK预初始化函数不会采集设备信息，也不会向友盟后台上报数据。
-// preInit预初始化函数耗时极少，不会影响App首次冷启动用户体验
-public static void preInit(Context context, String appkey, String channel)
-
         progressBar = findViewById(view, R.id.progress);
         eula = findViewById(view, R.id.eula);
         next = findViewById(view, R.id.next);
