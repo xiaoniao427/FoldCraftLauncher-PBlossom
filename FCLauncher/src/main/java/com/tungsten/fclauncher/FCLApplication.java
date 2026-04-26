6b736c6f79df35c09d669e84e8dae648c20330b7
xiaoniao427 <xiaoniao427@163.com>
2026-04-26 17:19:49 +0800

    修bug

--- FCLauncher/src/main/java/com/tungsten/fclauncher/FCLApplication.java
+++ FCLauncher/src/main/java/com/tungsten/fclauncher/FCLApplication.java
@@ -1,9 +1,14 @@
 package com.tungsten.fclauncher;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import com.umeng.commonsdk.UMConfigure;
 
-public class FCLApplication extends Application {
+public class FCLApplication extends Application implements Application.ActivityLifecycleCallbacks {
+    
+    private static Activity currentActivity;
+    
     @Override
     public void onCreate() {
         super.onCreate();
@@ -15,5 +20,47 @@ public class FCLApplication extends Application {
             UMConfigure.DEVICE_TYPE_PHONE, 
             "1853c4972a25c98245161c0bc6593e08"
         );
+        
+        // 注册Activity生命周期回调
+        registerActivityLifecycleCallbacks(this);
+    }
+    
+    public static Activity getCurrentActivity() {
+        return currentActivity;
+    }
+    
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+        if (currentActivity == activity) {
+            currentActivity = null;
+        }
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {}
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+        if (currentActivity == activity) {
+            currentActivity = null;
+        }
     }
 }
