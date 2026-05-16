2bb695ad797165be71b3423cbd8ea15e6717626e
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-05-16 19:37:45 +0800

    Update FCLApplication.java

--- FCLauncher/src/main/java/com/tungsten/fclauncher/FCLApplication.java
+++ FCLauncher/src/main/java/com/tungsten/fclauncher/FCLApplication.java
@@ -362,7 +362,7 @@ public class FCLApplication extends Application implements Application.ActivityL
         private final String watermarkText;
         private final Paint textPaint;
         private final float textSizeSp = 25f;      // 字体大小
-        private final float spacingDp = 150f;      // 用户期望间距
+        private final float spacingDp = 50f;      // 用户期望间距
         private float spacingPx;                   // 实际使用的间距（可能被修正）
         private float textSizePx;
         private float textWidth;
